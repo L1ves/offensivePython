89c49db4c1a5decf73ed0a9c4f0680f277b2d9e9
inc0gn1to <pro194@ya.ru>
2019-10-15 16:51:26 +0500

    Simple TCP server

---
+++ simpleTCP.py
@@ -0,0 +1,21 @@
+#simple TCP server 1
+import socket
+
+target_host = "cyber-danger.ru"
+target_port = 80
+
+#create socket object
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connect the client
+client.connect((target_host, target_port))
+
+#send some data
+
+client.send("GET / HTTP/1.1\r\nHost: cyber-danger.ru\r\n\r\n".encode())
+
+#recieve some data 
+response = client.recv(4096)
+
+print(response)
